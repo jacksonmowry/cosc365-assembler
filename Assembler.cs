4efa4397aac134c95ffd473b9c3e1038f8a37d7f
Tatiana Dehoff <tatianadehoff@gmail.com>
2025-04-01 13:19:54 -0400

    Fix to handle comments/labels, added all instructions to the switch statement

--- Assembler.cs
+++ Assembler.cs
@@ -48,28 +48,62 @@
         // You'll need to for branch/goto instructions
         for (int i = 0; i < source.Count(); i++)
         {
-            int? argOne = Utils.ParseInt(source[i].Tokens.ElementAtOrDefault(1));
+            // In some cases, first argument can be a label
+            string? argOneStr = source[i].Tokens.ElementAtOrDefault(1);
+            // Try parsing arguments as integers
+            int? argOne = Utils.ParseInt(argOneStr);
             int? argTwo = Utils.ParseInt(source[i].Tokens.ElementAtOrDefault(2));
+            // First argument is a string, try parsing a label
+            int offset = 0;
+            if (argOneStr != null && !argOne.HasValue) {
+                int labelAddress = 0;
+                if (labelMap.TryGetValue(argOneStr, out labelAddress)) {
+                    int currAddress = i * 4;
+                    offset = labelAddress - (currAddress + 4);
+                }
+            }
             Instruction.IInstruction ins = source[i].Tokens[0].ToLower() switch
             {
-                "exit" => new Exit(argOne),
-                "swap" => new Swap(argOne, argTwo),
-                "nop" => new Nop(),
-                "input" => new Input(),
+                "exit"    => new Exit(argOne),
+                "swap"    => new Swap(argOne, argTwo),
+                "nop"     => new Nop(),
+                "input"   => new Input(),
                 "stinput" => new StInput(argOne),
-                "debug" => new Debug(argOne),
-                "pop" => new Pop(argOne),
-                "add" => new Add(),
-                "sub" => new Sub(),
-                "mul" => new Mul(),
-                "div" => new Div(),
-                "rem" => new Rem(),
-                "and" => new And(),
-                "or" => new Or(),
-                "lsl" => new Lsl(),
-                "asr" => new Asr(),
-                "neg" => new Neg(),
-                "not" => new Not(),
+                "debug"   => new Debug(argOne),
+                "pop"     => new Pop(argOne),
+                "add"     => new Add(),
+                "sub"     => new Sub(),
+                "mul"     => new Mul(),
+                "div"     => new Div(),
+                "rem"     => new Rem(),
+                "and"     => new And(),
+                "or"      => new Or(),
+                // "xor"     => new Xor(),
+                "lsl"     => new Lsl(),
+                // "rsl"     => new Rsl(),
+                "asr"     => new Asr(),
+                "neg"     => new Neg(),
+                "not"     => new Not(),
+                "stprint" => new Stprint(argOne),
+                "call"    => new Call(offset),
+                "return"  => new Return(argOne),
+                "goto"    => new Goto(offset),
+                // "ifeq"    => new Ifeq(argOne),
+                // "ifne"    => new Ifne(argOne),
+                // "iflt"    => new Iflt(argOne),
+                // "ifgt"    => new Ifgt(argOne),
+                // "ifle"    => new Ifle(argOne),
+                // "ifge"    => new Ifge(argOne),
+                // "ifez"    => new Ifez(argOne),
+                // "ifnz"    => new Ifnz(argOne),
+                // "ifmi"    => new Ifmi(argOne),
+                // "ifpl"    => new Ifpl(argOne),
+                "dup"     => new Dup(argOne),
+                // "printh"  => new Printh(argOne),
+                // "printo"  => new Printo(argOne),
+                // "printb"  => new Printb(argOne),
+                // "dump"    => new Dump(),
+                "push"    => new Push(argOne),
                 _ => throw new Exception($"Unimplemented instruction {source[i].Tokens[0]}")
             };
 
