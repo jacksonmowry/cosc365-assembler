1f4ffa834637568ae6cc7e1f5893689aa51c8c4c
Tatiana Dehoff <tatianadehoff@gmail.com>
2025-04-02 00:01:33 -0400

    Fixed call, added goto

--- Instructions/Goto.cs
+++ Instructions/Goto.cs
@@ -1,14 +1,14 @@
 public class Goto : Instruction.IInstruction
 {
-    private readonly int _offset = 0;
+    private readonly int _offset;
 
-    public Goto(int? label)
+    public Goto(int offset)
     {
-
+        _offset = offset;
     }
 
     public int Encode()
     {
-        return (0b0111 << 28) | _offset;
+        return (0b0111 << 28) | (_offset & 0x0fffffff);
     }
 }
